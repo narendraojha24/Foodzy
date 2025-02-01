e8680a3bdc9beaefbcab3b0e21e7185982843bac
narendraojha24 <151997177+narendraojha24@users.noreply.github.com>
2025-02-01 21:20:18 +0530

    changes

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,14 +1,21 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 function App() {
   return (
-    <>
-      <div className='nanbar'>
+      <div className='app'>
         <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/order' element={<PlaceOrder/>} />
+        </Routes>
       </div>
-    </>
   )
 }
 
--- react/src/components/navbar/Navbar.jsx
+++ react/src/components/navbar/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 export default function Navbar() {
+  const[menu,setMenu]=useState("home");
   return (
-    <div>
-        <div>
-            bjdbwoiehfio3bvi3rv bdjcguoierf
-        </div>
+    <div className='navbar'>
+      <div className="logo">
+        <h1>Foodzy</h1></div>
+        <ul className='navbar-menu'>
+          <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
+          <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+          <li onClick={()=>setMenu("mobile-app")}className={menu==="mobile-app"?"active":""}>mobile-app</li>
+          <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
+          </ul>   
+          <div className="navbar-right">
+            <div className="box-icon"><i class='bx bx-search'></i></div>
+            <div className="navbar-search-icon">
+            <div className="box-icon"><i class='bx bxs-basket' ></i></div>
+              <div className="dot"></div>
+            </div>
+            <button className='signin'>Sign in</button>
+          </div> 
       
     </div>
   )
