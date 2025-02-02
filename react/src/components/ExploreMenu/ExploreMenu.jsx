57639bdd390d919dde21a7cc909050ee730bc9be
narendraojha24 <151997177+narendraojha24@users.noreply.github.com>
2025-02-02 18:36:05 +0530

    changes

--- react/src/components/ExploreMenu/ExploreMenu.jsx
+++ react/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
-export default function ExploreMenu() {
+export default function ExploreMenu({category,setCategory}) {
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1>Explore our menu</h1>
-        <p>nkdkcnirhnfi4rhj j dcjbirhfiy94rjbi hirfhyv9u4rn jbihciu49fu490fbv jccbihrif94rf cjbifvh4irfu94uf jcbeifh9i4uf94d  jbihfiuf9</p>
+        <p className='explore-menu-text'>nkdkcnirhnfi4rhj j dcjbirhfiy94rjbi hirfhyv9u4rn jbihciu49fu490fbv jccbihrif94rf cjbifvh4irfu94uf jcbeifh9i4uf94d  jbihfiuf9</p>
       <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
             return(
-                <div className="explore-menu-list-item">
-                    <img src={item.menu_image} alt="" />
+                <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} className="explore-menu-list-item">
+                    <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
                     <p>{item.menu_name}</p>
                 </div>
             )
@@ -19,6 +19,7 @@ export default function ExploreMenu() {
 
         )}
       </div>
+      <hr />
     </div>
   )
 }
