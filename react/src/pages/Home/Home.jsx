e697f4c9aed9c7ffabf3e6915edd1c37dc4e7efe
narendraojha24 <151997177+narendraojha24@users.noreply.github.com>
2025-02-02 13:25:48 +0530

    header

--- react/src/pages/Home/Home.jsx
+++ react/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../components/Header/Header'
 
 export default function Home() {
   return (
     <div>
+      <Header/>
       
     </div>
   )
