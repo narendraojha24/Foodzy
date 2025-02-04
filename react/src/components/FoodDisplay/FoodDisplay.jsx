ef4abbe9e441d86ac59d4060cf5be78879c8ca3a
narendraojha24 <151997177+narendraojha24@users.noreply.github.com>
2025-02-04 21:47:31 +0530

    pages

--- react/src/components/FoodDisplay/FoodDisplay.jsx
+++ react/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,17 +1,23 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
 
 export default function FoodDisplay({category}) {
 
     const {food_list} = useContext(StoreContext)
+
   return (
-    <div>
+
         <div className="food-display" id='food-display'>
             <h2>Top dishes near you</h2>
-            <div></div>
+            <div className='food-display-list'>
+              {food_list.map((item,index)=>{
+              return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+              
+              })}
+
+            </div>
         </div>
-      
-    </div>
   )
 }
