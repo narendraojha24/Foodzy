4f1b10777989804361e3afd832998a44605c6fa5
narendraojha24 <151997177+narendraojha24@users.noreply.github.com>
2025-02-14 16:54:36 +0530

    changes

--- react/src/components/Header/Header.jsx
+++ react/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className='header'>
         <div className="header-contents">
             <h1>Bringing Your Favorite Meals to Your Doorstep!</h1>
-            <p>Craving Something? Let Us Deliver Happiness!</p>
+            <p>Craving Something ? Let Us Deliver Happiness!</p>
             <button>view here</button>   
         </div>
       
