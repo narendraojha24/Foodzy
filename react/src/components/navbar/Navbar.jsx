152cdd14ea2b88e51eb6597719d8e0928be4466f
narendraojha24 <151997177+narendraojha24@users.noreply.github.com>
2025-02-22 23:29:57 +0530

    responsiveness

--- react/src/components/navbar/Navbar.jsx
+++ react/src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   const[menu,setMenu]=useState("home");
@@ -9,10 +10,10 @@ export default function Navbar() {
       <div className="logo">
         <h1>Foodzy</h1></div>
         <ul className='navbar-menu'>
-          <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
-          <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-          <li onClick={()=>setMenu("mobile-app")}className={menu==="mobile-app"?"active":""}>mobile-app</li>
-          <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
+          <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+          <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
+          <a href='#app-download' onClick={()=>setMenu("mobile-app")}className={menu==="mobile-app"?"active":""}>mobile-app</a>
+          <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
           </ul>   
           <div className="navbar-right">
             <div className="box-icon"><i class='bx bx-search'></i></div>
